2929bc14f8e8ac93d06fd77a92ba08dd88a9914a
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-05-27 16:53:10 +0530

    Add files via upload

---
+++ 2025-05-26 Day 8 /Codes/BubbleSort.m
@@ -0,0 +1,29 @@
+%% Bubble Sort -Psudo Code
+%% Start
+%% step 01: Take an array of numbers
+%% step 02:Repeat for all elements in the array
+%% -> Compare each pair of adjacent elements
+%% -> If the left element is greater than the right element 
+%%     -> Swap them
+%% step 03: Continue this process until the entire array is sorted 
+%% -> Large elements will "bubble up" to the end in each
+%% pass
+%% end
+%% End 
+
+arr = [64,34,25,12,22,11,90];
+n = length(array);
+
+for i=1:n-1
+    for j=1:n-i
+        if arr(j)>arr(j+1)
+            temp = arr(j);
+            arr(j)=arr(j+1);
+            arr(j+1)=temp;
+        end
+
+    end
+end
+
+disp("Sorted Array:");
+disp(arr);
