ff5132adbffc1a94e6ebb124d4897f8480d20c86
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-06-04 14:26:47 +0530

    Delete Graphs directory

--- Graphs/Codes/weightedUndirectedGraphs.m
+++
@@ -1,9 +0,0 @@
-%% Weighted Undirected Graph
-
-s = [1 2 2]; % start nodes
-t = [2 3 4]; % end nodes
-weights = [10 5 7];
-G = graph(s,t,weights);  % create a graph
-plot(G, 'EdgeLabel', G.Edges.Weight); %% plot the graph
-title('Weighted Undirected Graph'); % add a title
-
