8becb8d9340b2a0d77dcff3b0d669a4a913b9710
Ushani Dhanushika Ganhewage <ushanidhanushika2000@gmail.com>
2025-06-16 19:58:58 +0530

    Update exercise.m

--- DAA_Practicals/Graphs/Codes/exercise.m
+++ DAA_Practicals/Graphs/Codes/exercise.m
@@ -1,8 +1,22 @@
-s=[1 1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4 5 5 5 6  8 9 10 7];
-t=[6 2 11 10 5 6 11 7 3 7 11 4 8  8 11 5 9 9 10 11 11 11 11 11 11];
-G=graph(s,t);
-h=plot(G,'LineWidth',2.5);
-highlight(h,[1:11],'NodeColor','blue');
-highlight(h,11,1:10,'EdgeColor','magenta');
-highlight(h, [1 1 2 3 4], [2 5 3 4 5], 'EdgeColor', 'green');
-highlight(h,[1 6 2 7 3 8 4 9 5 10],[6 2 7 3 8 4 9 5 10 1],'EdgeColor', 'red');
+
+s = [1 2 3 4 5 11 11 11 11 11 11 11 11 11 11 8 8 7 7 6 6 10 10 9 9];
+t = [2 3 4 5 1  1  2  3  4  5  6  7  8  9 10  4  3  3  2  1  2  1  5  4  5];
+
+G1 = graph(s, t);
+h = plot(G1);
+
+greenEdges = [1 2; 2 3; 3 4; 4 5; 5 1];
+magentaEdges = [1 11; 2 11; 3 11; 4 11; 5 11; 6 11; 7 11; 8 11; 9 11; 10 11];
+redEdges = [1 10; 10 5; 5 9; 9 4; 4 8; 8 3; 3 7; 7 2; 2 6; 6 1];
+
+for i = 1:size(greenEdges,1)
+    highlight(h, greenEdges(i,1), greenEdges(i,2), 'EdgeColor', 'green');
+end
+
+for i = 1:size(magentaEdges,1)
+    highlight(h, magentaEdges(i,1), magentaEdges(i,2), 'EdgeColor', 'magenta');
+end
+
+for i = 1:size(redEdges,1)
+    highlight(h, redEdges(i,1), redEdges(i,2), 'EdgeColor', 'red');
+end
